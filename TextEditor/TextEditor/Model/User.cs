bae5bb8ab9cc9d8b5305d38752e6380df7891170
trofimchuk <trofimchuk@itstep.academy>
2021-11-22 10:02:39 +0200

    Start new project TextEditor

---
+++ TextEditor/TextEditor/Model/User.cs
@@ -0,0 +1,21 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextEditor.Model
+{
+    public class User
+    {
+        [PrimaryKey, AutoIncrement]
+        public uint Id { get; set; }
+        [MaxLength(30)]
+        public string FName { get; set; }
+        [MaxLength(40)]
+        public string LName { get; set; }
+        public string Username { get; set; }
+        public string  Password { get; set; }
+    }
+}
